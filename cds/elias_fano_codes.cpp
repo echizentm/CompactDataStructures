1832420d10a31206dd09dad101667f3258e17aa2
echizen_tm <echizentm@gmail.com>
2017-02-15 23:32:54 +0900

    implement elias fano codes class.

--- cds/elias_fano_codes.cpp
+++ cds/elias_fano_codes.cpp
@@ -2,19 +2,34 @@
 
 
 namespace cds {
-    elias_fano_codes::elias_fano_codes() {
+    elias_fano_codes::elias_fano_codes() : marker(1) {
         this->size = 0;
+        this->marker.resize(1);
+        this->marker.write(0, 1);
     }
 
     unsigned int elias_fano_codes::vector_size() {
-        return 0;
+        return this->bv.vector_size() + this->marker.vector_size();
     }
 
     unsigned int elias_fano_codes::read(unsigned int index) {
-        return 0;
+        unsigned int begin = this->marker.search(index + 1);
+        unsigned int end = this->marker.search(index + 2);
+
+        unsigned int value = this->bv.bits_read(begin, end);
+        return (value | (1 << (end - begin))) - 2;
     }
 
     void elias_fano_codes::push_back(unsigned int value) {
+        this->size++;
 
+        value += 2;
+        unsigned int length = bits_length(value) - 1;
+        value &= ((1 << length) - 1);
+        this->bv.resize(this->bv.size + length);
+        this->marker.resize(this->marker.size + length);
+
+        this->bv.bits_write(this->bv.size - length, this->bv.size, value);
+        this->marker.write(this->marker.size - 1, 1);
     }
 }
--- cds/elias_fano_codes.h
+++ cds/elias_fano_codes.h
@@ -1,8 +1,13 @@
 #pragma once
+#include "bit_vector.h"
+#include "partial_sums.h"
 
 
 namespace cds {
     class elias_fano_codes {
+        bit_vector bv;
+        partial_sums marker;
+
     public:
         unsigned int size;
 
